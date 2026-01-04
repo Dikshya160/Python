70da23d498cfd565fabca149c3ed8f7b4162abd8
Dikshya160 <dikshyasharma602@gmail.com>
2026-01-04 15:04:24 +0545

    first commit

---
+++ lecture0ne.py
@@ -0,0 +1,33 @@
+# name=input("Enter your name:")
+# age=int(input("Enter your age:"))
+# Marks= float(input("Enter your marks:"))
+
+# print("name=", name)
+# print(type(name))
+# print("age", age)
+# print(type(age))
+# print("Marks", Marks)
+# print((type(Marks)))
+
+
+
+#Que
+# num1=int(input("Enter first number:"))
+# num2=int(input("Enter second number:"))
+# sum=num1 + num2
+# print("sum=", sum)
+
+
+# a=int(input("Enter the first length of a square:"))
+# b=int(input("Enter the second length of a square:"))
+# print("area=", a*b)
+
+
+# a=float(input("Enter a first number:"))
+# b=float(input("Enter a second number:"))
+# print("average", (a+b)/2)
+
+
+a=int(input("Enter a first number:"))
+b=int(input("Enter a second number:"))
+print("ans:",a>=b)
