068eab5d9faa7b930b33143f520d6632a3761b97
Dikshya160 <dikshyasharma602@gmail.com>
2026-01-07 16:36:30 +0545

    lectureTwo.py

---
+++ lectureTwo.py
@@ -0,0 +1,118 @@
+#     STRING CONCEPT
+# string=("dikshya \t  sharma")
+# print(len(string))
+# string2=("lala")
+# len2=len(string2)
+# print(len2)
+# print("concatenation=", string + string2)
+
+
+
+          #INDEXING
+# str="dikshya sharma"
+# print(str[1])
+
+# var="DOOR"
+# ch=var[2]
+# print(ch)
+
+
+# a="apple ball"
+# print(a[5])
+
+
+ 
+            #sLICING
+# str="Dikshya"
+# print(str[1:4])
+# print(str[:3]) 
+# print(str[1:])
+# print(str[-7: -1 ])
+
+
+
+
+                 #STRING FUNCTION
+# var= "i am a student, i read in class eight"
+# #endswith() function
+# print(var.endswith("ght")) 
+# print(var.endswith("ing"))
+
+# #capitalize() function
+# print(var.capitalize()) #only capitalize the first letter
+
+# #replace("old", "new") function
+# print(var.replace("i","I" ))  #replace every letter
+
+# #find("word") function
+# print(var.find("a")) #give indexing of the first known
+
+# #count("word") function
+# print(var.count("a")) #no of time it came
+
+
+
+       #QUE
+# name=input("Enter your name:")
+# print("Name:",name)
+# print("lenth of your name:",len(name))
+
+# str="$DIK$SHYA$"
+# print("no of occurance:",str.count("$"))
+
+
+     #CONDITIONAL STATEMENT   if-elif-else:
+
+# marks= int(input("Enter your marks:"))
+# if(marks>=90):
+#     print("A")
+# elif(90>marks and marks<=80):
+#     print("B")
+# elif(80>marks and marks<=70):
+#     print("C")
+
+# elif(70>marks and marks<=60):
+#    print("D")
+
+# else:
+#     print("fail")
+      
+
+         #NESTING
+# age=20
+
+# if(age>=13):
+#     if(age>=20):
+#         print("not a teen")
+#     print(" you are a teen")
+
+# else:
+#     print("you are adult")
+
+
+
+# num=33
+# if(num%2==0):
+#     print("even")
+# else:
+#     print("odd")
+
+    # #find greater among five number
+    # a= int(input("Enter your first number:"))
+    # b= int(input("Enter your second number:"))
+    # c= int(input("Enter your third number:"))
+    # d= int(input("Enter your fourth number:"))
+    # e= int(input("Enter your five  number:"))
+    # if(a>b and a>c and a>d and a>e):
+    #     print(" greater = a")
+    # elif(b>c and b>d and b>e):
+    #     print("greater = b")
+    # elif(c>d and c>e):
+    #     print("c")
+    # elif(d>e):
+    #     print("d")
+    # else:
+    #     print("e")
+     
+
+  
