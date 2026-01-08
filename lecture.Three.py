bfc2fc1db81f990128455d505ff8cdaefc4a753b
Dikshya160 <dikshyasharma602@gmail.com>
2026-01-08 12:56:46 +0545

    Third commit

---
+++ lecture.Three.py
@@ -0,0 +1,98 @@
+#               #List & Tuple
+# marks=[9,19,80.67, 60.55,58]
+# print(marks)
+# print(type(marks))
+# print(marks[2])
+
+
+
+    #   mutable(List) and Inmutable(string)
+# str="Apple"
+# print(str[0])
+# str[0]="B"
+# print(str[0])    #Error dinxa for string
+
+# list=["Apple", "Ball"]
+# print(list[1])
+# list[1]="Cat"
+# print(list[1])           #it change value for list
+
+
+
+
+
+
+                     #List methods
+#list=[1,3,2]
+# print( list.append(5) )
+
+#list.sort()
+#print(list)
+
+#list.sort(reverse=True)
+#print(list)
+
+# list=[1,3,2]
+# list.reverse()
+# print(list)
+
+#list.insert(1,5)
+#print(list)
+
+
+# list=[2,3,1,5,1]
+ # list.remove(1)
+ # print(list)
+
+# list.pop(3)
+# print(list)
+
+
+
+                                    #TUPLES
+# tup=("a","doll", 5, 9, "a")
+# print(tup)
+# print(type(tup))
+
+# # print(tup[1:3])
+# print(tup.index("doll")) 
+# print(tup.count("a"))
+
+
+
+
+
+
+
+
+
+
+# movie=[]
+# mov1=input("Enter a first movie:")
+# mov2=input("Enter a second movie:")
+# mov3=input("Enter a third movie:")
+# movie.append(mov1)
+# movie.append(mov2)
+# movie.append(mov3)
+# print(movie)
+
+
+# num=[1,2,3,2,1]
+# coppy=num.copy()
+# print(coppy)
+
+# coppy.reverse()
+
+# if (num == coppy):
+#     print("True")
+# else:
+#     print("false")
+
+
+
+
+num="RAm, shyam, hari "
+
+list=num.split()
+print(list)
+print(type(list))
